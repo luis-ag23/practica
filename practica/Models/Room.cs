750b01476c6f5bf875e7e5021cf645c01c07ba56
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2025-10-02 21:26:06 -0400

    adding

--- practica/Models/Room.cs
+++ practica/Models/Room.cs
@@ -1,6 +1,33 @@
-﻿namespace practica.Models
+﻿using System.ComponentModel.DataAnnotations;
+public class Room
 {
-    public class Room
-    {
-    }
+    public Guid Id { get; set; }
+    [Required]
+    public string RoomNumber { get; set; } = string.Empty;
+    [Required]
+    public string RoomType { get; set; } = string.Empty;
+    [Required]
+    public decimal PricePerNight { get; set; }
+    [Required]
+    public bool Avaliable { get; set; }
+}
+
+public record CreateRoomDto
+{
+    [Required]
+    public string RoomNumber { get; set;} = string.Empty;
+
+    [Required]
+    public string RoomType { get; set;} = string.Empty ;
+    [Required]
+    public decimal PricePerNight { get;set;}
+    [Required]
+    public bool Avaliable { get; set; }
+}
+public record UpdateRoomDto
+{
+    public required string RoomNumber { get; set;}
+    public required string RoomType { get; set;}
+    public decimal PricePerNight { get; set;}
+    public required bool Avaliable { get; set;}
 }
