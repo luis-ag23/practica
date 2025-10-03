da4429759f07d0b865df5dbf8b1d6869a298a669
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2025-10-03 00:22:06 -0400

    feat: create model reservation

---
+++ practica/Models/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+public class Reservation
+{
+    public Guid Id { get; set; }
+    [Required]
+    public Guid RoomId { get; set; }
+    [Required]
+    public Guid CustomerId { get; set; }
+    [Required]
+    public DateTime CheckInDate { get; set; }
+    [Required]
+    public DateTime CheckOutDate { get; set; }
+    [Required]
+    public string Status { get; set; } = string.Empty;
+}
+public record CreateReservationDto
+{
+    [Required]
+    public DateTime CheckInDate { get; set; }
+    [Required]
+    public DateTime CheckOutDate { get; set; }
+    [Required]
+    public string Status { get; set; } = string.Empty;
+}
+public record UpdateReservationDto
+{
+    public required DateTime CheckInDate { get; set; }
+    public required DateTime CheckOutDate { get; set; }
+    public required string Status { get; set; }
+}
