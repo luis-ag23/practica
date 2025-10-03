6e40c39c07d8df75b503c6146c7a4cc0dec381b0
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2025-10-03 00:32:40 -0400

    feat: adding getOne

---
+++ practica/Controllers/ReservationsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace practica.Controllers
+{
+    [ApiController]
+    [Route("api/{controller}")]
+    public class ReservationsController
+    {
+        private static readonly List<Reservation> _reservations = new()
+        {
+            new Reservation{
+                Id=Guid.NewGuid(),RoomId=Guid.NewGuid(),CustomerId=Guid.NewGuid(),CheckInDate=DateTime.Parse("12-02-2022"),CheckOutDate=DateTime.Parse("13-02-2022"),
+                Status="confirmed"},
+        };
+    }
+}
