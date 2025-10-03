344521eb53662f5940a78a4ef7d97c93956bb759
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2025-10-03 13:19:46 -0400

    feat: adding

---
+++ practica/Models/Payment.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+public class Payment
+{
+    public Guid Id { get; set; }
+    public Guid ReservationId { get; set; }
+    [Required]
+    public decimal Amount { get; set; }
+
+    [Required]
+    public DateTime PaymentDate { get; set; }
+    [Required]
+    public string PaymentStatus { get; set; } = string.Empty;
+}
+
+public record CreatePaymentDto
+{
+    [Required]
+    public decimal Amount { get; set; }
+    [Required]
+    public DateTime PaymentDate { get; set; }
+    [Required]
+    public string PaymentStatus { get; set; } = string.Empty;
+}
+
+public record UpdatePaymentDto
+{
+    public required decimal Amount { get; set; }
+    public required DateTime PaymentDate { get; set; }
+    public required string PaymentStatus { get; set; }
+}
